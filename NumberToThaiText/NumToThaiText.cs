09f033c58c5f5b278a810a1bd4621d7272b1cb82
gone <nattlao@gmail.com>
2021-08-05 14:43:05 +0700

    no message

---
+++ NumberToThaiText/NumToThaiText.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberToThaiText
+{
+    public class NumToThaiText : CodeActivity
+    {
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<decimal> Number { get; set; }
+
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<bool> HasBahtSuffix { get; set; }
+
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<bool> HasSatangSuffix { get; set; }
+
+        [Category("Output")]
+        public OutArgument<string> ThaiIntegerText { get; set; }
+
+        [Category("Output")]
+        public OutArgument<string> ThaiDecimalText { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var converter = new NumToThaiTextConverter().Convert(Number.Get(context).ToString()
+                , Boolean.Parse(HasBahtSuffix.Get(context).ToString())
+                , Boolean.Parse(HasSatangSuffix.Get(context).ToString()));           
+
+            ThaiIntegerText.Set(context, converter.Interger);
+            ThaiDecimalText.Set(context, converter.Satang);
+        }
+    }
+}
